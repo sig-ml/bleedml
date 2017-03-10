516e29cf5e3fd1b6408e2ab0b497c9c801afbfd9
Arjoonn Sharma <arjoonn.94@gmail.com>
2017-03-10 21:38:59 +0530

    parameter change for gcForets

--- bleedml/classifiers.py
+++ bleedml/classifiers.py
@@ -10,8 +10,8 @@ from sklearn.metrics import confusion_matrix, accuracy_score
 
 class GCForest(BaseEstimator, ClassifierMixin):
     "From https://arxiv.org/abs/1702.08835"
-    def __init__(self, n_estimators=10, n_layers=3, cv=3):
-        self.n_estimators = n_estimators
+    def __init__(self, n_layers=3, cv=3, n_estimators_in_layers=[[1000]*4, [1000]*4, [1000]*4]):
+        self.n_estimators_in_layers = n_estimators_in_layers
         self.n_layers = n_layers
         self.cv = cv
 
@@ -21,10 +21,11 @@ class GCForest(BaseEstimator, ClassifierMixin):
         layers, input_data = [], X
         classes = unique_labels(y)
         for i in range(self.n_layers):
-            forests = [RandomForestClassifier(max_features=1, n_estimators=self.n_estimators),
-                    RandomForestClassifier(max_features=1, n_estimators=self.n_estimators),
-                    RandomForestClassifier(n_estimators=self.n_estimators),
-                    RandomForestClassifier(n_estimators=self.n_estimators)]
+            f1est, f2est, f3est, f4est = self.n_estimators_in_layers[i]
+            forests = [RandomForestClassifier(max_features=1, n_estimators=f1est, min_samples_split=10),
+                    RandomForestClassifier(max_features=1, n_estimators=f2est, min_samples_split=10),
+                    RandomForestClassifier(n_estimators=f3est),
+                    RandomForestClassifier(n_estimators=f4est)]
             predictions = [cross_val_predict(f, input_data, y, method='predict_proba', cv=self.cv) for f in forests]
             _ = [forest.fit(input_data, y) for forest in forests] # Fit the forest
             layers.append(forests)
