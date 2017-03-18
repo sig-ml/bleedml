6190e7da3dea3d61f30899208fa031ef6ba3fd5a
Arjoonn Sharma <arjoonn.94@gmail.com>
2017-03-18 09:47:58 +0530

    cascade forest update

--- bleedml/classifiers.py
+++ bleedml/classifiers.py
@@ -10,20 +10,19 @@ from sklearn.metrics import confusion_matrix, accuracy_score
 
 class CascadeForest(BaseEstimator, ClassifierMixin):
     "From https://arxiv.org/abs/1702.08835"
-    def __init__(self, cv=None, n_estimators=None, scoring=None, tolerance=None, validation_fraction=None):
+    def __init__(self, cv=3, n_estimators=100, scoring=accuracy_score, tolerance=0.01, validation_fraction=0.8):
         """
-        cv                  : how many folds to use for prediction generation in each layer?
-        n_estimators        : how many estimators in each layer's forest
-        scoring             : what score to minimize for?
-        tolerance           : what tolerance to stop at?
-        validation_fraction : what fraction of data to use for validation during cascade growth?
+        cv                  :(3) how many folds to use for prediction generation in each layer?
+        n_estimators        :(100) how many estimators in each layer's forest
+        scoring             :(accuracy) what score to minimize for?
+        tolerance           :(0.01) what tolerance to stop at?
+        validation_fraction :(0.8) what fraction of data to use for validation during cascade growth?
         """
-        self.n_estimators = n_estimators if n_estimators is not None else 100
-        self.cv = cv if cv is not None else 3
-        self.scoring = scoring if scoring is not None else accuracy_score
-        self.tolerance = tolerance if tolerance is not None else 0.01
-        self.validation_fraction = validation_fraction if validation_fraction is not None else 0.8
-        self._estimator_type = 'classifier'
+        self.n_estimators = n_estimators
+        self.cv = cv
+        self.scoring = scoring
+        self.tolerance = tolerance
+        self.validation_fraction = validation_fraction
 
     def fit(self, X, y):
         # Check data
@@ -38,10 +37,10 @@ class CascadeForest(BaseEstimator, ClassifierMixin):
         self.scores_ = []
 
         # First layer
-        forests = [RandomForestClassifier(max_features=1, n_estimators=self.n_estimators, min_samples_split=10),  # Complete random
-                    RandomForestClassifier(max_features=1, n_estimators=self.n_estimators, min_samples_split=10),  # Complete random
-                    RandomForestClassifier(n_estimators=self.n_estimators),
-                    RandomForestClassifier(n_estimators=self.n_estimators)]
+        forests = [RandomForestClassifier(max_features=1, n_estimators=self.n_estimators, min_samples_split=10, criterion='gini', n_jobs=-1),  # Complete random
+                    RandomForestClassifier(max_features=1, n_estimators=self.n_estimators, min_samples_split=10, criterion='gini', n_jobs=-1),  # Complete random
+                    RandomForestClassifier(n_estimators=self.n_estimators, n_jobs=-1),
+                    RandomForestClassifier(n_estimators=self.n_estimators, n_jobs=-1)]
         _ = [f.fit(inp_tr, y_tr) for f in forests]
         p_vl = [f.predict_proba(inp_vl) for f in forests]
         labels = [self.classes_[i] for i in np.argmax(np.array(p_vl).mean(axis=0), axis=1)]
@@ -54,21 +53,22 @@ class CascadeForest(BaseEstimator, ClassifierMixin):
         last_score = score
         inp_tr, inp_vl = np.concatenate([X_tr]+p_tr, axis=1), np.concatenate([X_vl]+p_vl, axis=1)
         while True:  # Grow cascade
-            forests = [RandomForestClassifier(max_features=1, n_estimators=self.n_estimators, min_samples_split=10),  # Complete random
-                    RandomForestClassifier(max_features=1, n_estimators=self.n_estimators, min_samples_split=10),  # Complete random
-                    RandomForestClassifier(n_estimators=self.n_estimators),
-                    RandomForestClassifier(n_estimators=self.n_estimators)]
+            forests = [RandomForestClassifier(max_features=1, n_estimators=self.n_estimators, min_samples_split=10, criterion='gini', n_jobs=-1),  # Complete random
+                    RandomForestClassifier(max_features=1, n_estimators=self.n_estimators, min_samples_split=10, criterion='gini', n_jobs=-1),  # Complete random
+                    RandomForestClassifier(n_estimators=self.n_estimators, n_jobs=-1),
+                    RandomForestClassifier(n_estimators=self.n_estimators, n_jobs=-1)]
             _ = [forest.fit(inp_tr, y_tr) for forest in forests] # Fit the forest
             p_vl = [forest.predict_proba(inp_vl) for forest in forests]
             labels = [self.classes_[i] for i in np.argmax(np.array(p_vl).mean(axis=0), axis=1)]
             score = self.scoring(y_vl, labels)
 
-            if score - last_score >= self.tolerance:
+            if score - last_score > self.tolerance:
                 self.layers_.append(forests)
                 p_tr = [cross_val_predict(f, inp_tr, y_tr, cv=self.cv, method='predict_proba') for f in forests]
                 inp_tr, inp_vl = np.concatenate([X_tr]+p_tr, axis=1), np.concatenate([X_vl]+p_vl, axis=1)
                 self.scores_.append(score)
                 last_score = score
+                print(self.scores_)
             else:
                 break
         # Retrain on entire dataset
@@ -84,12 +84,17 @@ class CascadeForest(BaseEstimator, ClassifierMixin):
         X = check_array(X)
         inp_ = X
         for forests in self.layers_:
-            p = [f.predict_proba(X) for f in forests]
+            p = [f.predict_proba(inp_) for f in forests]
             inp_ = np.concatenate([X] + p, axis=1)
         avg_p = np.array(p).mean(axis=0)
         return avg_p
 
     def predict(self, X):
+        X = check_array(X)
         avg_p = self.predict_proba(X)
         labels = np.array([self.classes_[i] for i in np.argmax(avg_p, axis=1)])
         return labels
+
+if __name__ == '__main__':
+    from sklearn.utils.estimator_checks import check_estimator
+    check_estimator(CascadeForest)
